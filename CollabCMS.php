133dc1eb0698b10361a47e3e213c9f293e3e0e15
ContentsViewer <fivetwothreesix@gmail.com>
2019-08-29 15:15:14 +0900

    mark, em, strongデザイン修正

--- CollabCMS.php
+++ CollabCMS.php
@@ -19,7 +19,7 @@ define('CACHE_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'Cache');
 
 define('PASSWORD_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'Password');
 
-define('VERSION', '2019. Aug.27.');
+define('VERSION', '2019. Aug.31.');
 
 // echo ROOT_DIR;
 // echo MODULE_DIR;
