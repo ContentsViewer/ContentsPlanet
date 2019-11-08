d371d08234c3cf3301d4a22bdc7bb797870f7b34
IOE <fivetwothreesix@gmail.com>
2019-11-08 13:30:40 +0900

    検索UI改良

--- Module/ContentsViewerUtils.php
+++ Module/ContentsViewerUtils.php
@@ -116,7 +116,7 @@ function CreateSearchOverlay(){
     <div id='search-overlay'>
         <div class='overlay-mask'></div>
         <div class='overlay-header'>
-            <form class='search-box'>
+            <form class='search-box' onsubmit='document.activeElement.blur(); return false;'>
                 <input id='search-box-input' placeholder='ContentsViewer内を検索' oninput='OnInputSearchBox()'>
                 <div id='search-box-input-clear-button' class='clear' onclick='OnClickSearchBoxInputClearButton()'><div class='clear-icon'></div></div>
             </form>
