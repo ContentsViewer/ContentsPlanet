242cd28f5618db35a4d2f5afe185a694bf5cb2a5
IOE <fivetwothreesix@gmail.com>
2020-04-28 14:55:45 +0900

    関連ページで, 何と関連しているか提示

--- Frontend/related-viewer.php
+++ Frontend/related-viewer.php
@@ -37,85 +37,57 @@ $indexFilePath = ContentsDatabaseManager::GetRelatedIndexFileName($contentPath);
 
 SearchEngine\Searcher::LoadIndex($indexFilePath);
 
-// --- 関連コンテンツの検索
+// === 関連コンテンツの検索 =================================================
 
-$suggestions = [];
+// $suggestions = [];
 
 // "<title> <parent.title>" で検索
 // ただし, parent は rootではない
-$query = NotBlankText([$currentContent->title, basename($currentContent->path)]) . 
+$titleQuery = NotBlankText([$currentContent->title, basename($currentContent->path)]) . 
     (($parent === false || $parent->IsRoot()) ? '' : ' ' . NotBlankText([$parent->title, basename($parent->path)]) );
 
-$workSuggestions = SearchEngine\Searcher::Search($query);
+$titleSuggestions = SearchEngine\Searcher::Search($titleQuery);
 
 // score 0.5 未満 は除外 
-foreach($workSuggestions as $i => $suggestion){
+foreach($titleSuggestions as $i => $suggestion){
     if($suggestion['score'] < 0.5){
-        unset($workSuggestions[$i]);
+        unset($titleSuggestions[$i]);
     }
 }
 // Debug::Log($workSuggestions);
-$suggestions = array_merge($suggestions, $workSuggestions);
-
+// $suggestions = array_merge($suggestions, $workSuggestions);
 
 // "<tag1> <tag2> <tag3> ..." で検索
-$query = '';
+$tagQuery = '';
 foreach($currentContent->tags as $tag){
     if(!in_array($tag, array('noindex', 'noindex-latest', '編集中', 'editing'))){
-        $query .= $tag . ' ';
+        $tagQuery .= $tag . ' ';
     }
 }
-$workSuggestions = SearchEngine\Searcher::Search($query);
+$tagSuggestions = SearchEngine\Searcher::Search($tagQuery);
 
 // 全タグをAND検索したとき, score 0.3 未満のものは除外
-foreach($workSuggestions as $i => $suggestion){
+foreach($tagSuggestions as $i => $suggestion){
     if($suggestion['score'] < 0.3){
-        unset($workSuggestions[$i]);
+        unset($tagSuggestions[$i]);
     }
 }
-$suggestions = array_merge($suggestions, $workSuggestions);
+// $suggestions = array_merge($suggestions, $workSuggestions);
 
-
-// 重複を除外
-$uniqueKeys = [];
-foreach($suggestions as $i => $suggestion){
-    if(array_key_exists($suggestion['id'], $uniqueKeys)){
-        unset($suggestions[$i]);
-        continue;
-    }
-    $uniqueKeys[$suggestion['id']] = true;    
-}
-
-/*
-$thisDirectory = $currentContent->path;
-// $rootContent = $currentContent;
-// while($rootContent !== false){
-//     $content = $rootContent->Parent();
-//     if($content === false){
-//         break;
+// // 重複を除外
+// $uniqueKeys = [];
+// foreach($suggestions as $i => $suggestion){
+//     if(array_key_exists($suggestion['id'], $uniqueKeys)){
+//         unset($suggestions[$i]);
+//         continue;
 //     }
-//     $rootContent = $content;
+//     $uniqueKeys[$suggestion['id']] = true;    
 // }
 
-// $rootContentsFolder = dirname($rootContent->path);
-$rootContentsFolder = ContentsDatabaseManager::GetRootContentsFolder($currentContent->path);
-for($i = 0; $i < 2; $i++){
-    $dirname = dirname($thisDirectory);
-    if($dirname == $rootContentsFolder){
-        break;
-    }
-    $thisDirectory = $dirname;
-}
+$titleSuggestions = SelectDifferentDirectoryContents($titleSuggestions, $currentContent->path);
+$tagSuggestions = SelectDifferentDirectoryContents($tagSuggestions, $currentContent->path);
 
-*/
-
-foreach($suggestions as $i => $suggestion){
-    $steps = CountSteps($suggestion['id'], $currentContent->path);
-    if($steps !== false && $steps < 5){
-        unset($suggestions[$i]);
-    }
-}
-
+// End 関連コンテンツの検索 =================================================
 
 // === ページ内容設定 =======================================================
 
@@ -139,34 +111,34 @@ $vars['pageTabs'] = [
     ['selected' => false, 'innerHTML' => '<a href="' . CreateDirectoryHREF(dirname($contentPath)) .'">ディレクトリ</a>'],
     ['selected' => true, 'innerHTML' => '<a href="' . CreateContentHREF($currentContent->path) .'?related">関連</a>']];
 
-$vars['contentBody'] = '';
 
-// child list の設定
 $vars['childList'] = []; // [ ['title' => '', 'summary' => '', 'url' => ''], ... ]
 
-$content = new Content();
-foreach($suggestions as $suggestion){
-    if($content->SetContent($suggestion['id'])){
-        $parent = $content->Parent();
-        $vars['childList'][] = [
-            'title' => NotBlankText([$content->title, basename($content->path)]) . 
-                ($parent === false ? '' : ' | ' . NotBlankText([$parent->title, basename($parent->path)])), 
-            'summary' => GetDecodedText($content)['summary'], 
-            'url' => CreateContentHREF($content->path)
-        ];
-    }
+$body = '';
+
+if(count($titleSuggestions) > 0){
+    $body .= '<h3>タイトル「' . trim($titleQuery) . '」に関連する</h3>';
+    $body .= CreateSuggestedContentList($titleSuggestions);
 }
 
-if(count($vars['childList']) > 0){
+if(count($tagSuggestions) > 0){
+    $body .= '<h3>タグ「' . trim($tagQuery) . '」に関連する</h3>';
+    $body .= CreateSuggestedContentList($tagSuggestions);
+}
+
+
+if(count($titleSuggestions) + count($tagSuggestions) > 0){
     $vars['contentSummary'] = '<p>「コンテンツ: <a href="' . CreateContentHREF($currentContent->path) . '">' .  
     NotBlankText([$currentContent->title, basename($currentContent->path)]) .
-    '</a>」と関連するコンテンツが, 別階層で<em>' . count($vars['childList']) .'件</em>見つかりました.</p>';
+    '</a>」と関連するコンテンツが, 別階層で<em>' . (count($titleSuggestions) + count($tagSuggestions)) .'件</em>見つかりました.</p>';
 }
 else{
     $vars['contentSummary'] = '<p>「コンテンツ: <a href="' . CreateContentHREF($currentContent->path) . '">' .  
     NotBlankText([$currentContent->title, basename($currentContent->path)]) .
     '</a>」と関連するコンテンツが, 別階層で見つかりませんでした.</p>';
 }
+
+$vars['contentBody'] = $body;
 
 // ビルド時間計測 終了
 $stopwatch->Stop();
@@ -224,4 +196,32 @@ function CountSteps($pathFrom, $pathTo){
 
     // Debug::Log($steps);
     return $steps;
+}
+
+function SelectDifferentDirectoryContents($suggestions, $currentContentPath){
+    foreach($suggestions as $i => $suggestion){
+        $steps = CountSteps($suggestion['id'], $currentContentPath);
+        if($steps !== false && $steps < 5){
+            unset($suggestions[$i]);
+        }
+    }
+    return $suggestions;
+}
+
+function CreateSuggestedContentList($suggestions){
+    $html = '<ul class="child-list">';
+
+    $content = new Content();
+    foreach ($suggestions as $suggestion) {
+        if($content->SetContent($suggestion['id'])){
+            $parent = $content->Parent();
+            $html .= '<li><div><div class="child-title">' .
+                '<a href="'. CreateContentHREF($content->path) . '">' . 
+                NotBlankText([$content->title, basename($content->path)]) . 
+                ($parent === false ? '' : ' | ' . NotBlankText([$parent->title, basename($parent->path)])) . '</a>' .
+                '</div><div class="child-summary">' . GetDecodedText($content)['summary'] . '</div></div></li>';
+        }
+    }
+    $html .= '</ul>';
+    return $html;
 }
