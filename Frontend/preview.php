a57536ec0918e7cefd7aad270b410114ea9ff33a
IOE <fivetwothreesix@gmail.com>
2020-08-27 01:18:17 +0900

    refactoring

--- Frontend/preview.php
+++ Frontend/preview.php
@@ -67,35 +67,8 @@ if($vars['layerName'] === false){
   <?=ContentTextParser::Parse($plainText, $vars['contentPath'], $context);?>
 
   <!-- SyntaxHighlighter 有効化 -->
-  <script>
-  SyntaxHighlighter.autoloader(
-    'applescript            <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushAppleScript.js',
-    'actionscript3 as3      <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushAS3.js',
-    'bash shell             <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushBash.js',
-    'coldfusion cf          <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushColdFusion.js',
-    'cpp c                  <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushCpp.js',
-    'c# c-sharp csharp      <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushCSharp.js',
-    'css                    <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushCss.js',
-    'delphi pascal          <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushDelphi.js',
-    'diff patch pas         <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushDiff.js',
-    'erl erlang             <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushErlang.js',
-    'groovy                 <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushGroovy.js',
-    'java                   <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushJava.js',
-    'jfx javafx             <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushJavaFX.js',
-    'js jscript javascript  <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushJScript.js',
-    'perl pl                <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushPerl.js',
-    'php                    <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushPhp.js',
-    'text plain             <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushPlain.js',
-    'py python              <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushPython.js',
-    'ruby rails ror rb      <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushRuby.js',
-    'sass scss              <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushSass.js',
-    'scala                  <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushScala.js',
-    'sql                    <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushSql.js',
-    'xml xhtml xslt html    <?=CLIENT_URI?>/syntaxhighlighter/scripts/shBrushXml.js'
-  );
-  SyntaxHighlighter.defaults['gutter'] = false;
-  SyntaxHighlighter.all();
-  </script>
+  <script type="text/javascript" src="<?=CLIENT_URI?>/syntaxhighlighter-loader/loader.js"></script>
+  <script>loadSyntaxHighlighter("<?=CLIENT_URI?>");</script>
 </body>
 
 </html>
