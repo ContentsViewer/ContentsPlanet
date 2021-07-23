485a1840f6f5af1a132fef3a2923eca35f7be6db
IOE <fivetwothreesix@gmail.com>
2021-07-23 17:36:40 +0900

    fixed bug of parsing code block after def list.

--- Module/OutlineText.php
+++ Module/OutlineText.php
@@ -1537,9 +1537,14 @@ class Parser {
 
             // End インデントの変化を見る ---
             
+            // 行頭の前処理
+            // 次の要素が, 文法外要素であるかどうかにかかわらず, 必ず実行される.
+            $output .= static::CallbackEventFuncs(static::$onPreBeginLineParserFuncList, $context);
+
             // 空文字の時
             // インデント値はあるが, 空文字
-            // その次がインラインコード, html要素のときに起こる.
+            // その次がインラインコード, html要素, コードブロックのときに起こる.
+            // 次がExceptElements(文法外要素)の時に起こる. 
             if ($currentMorph["content"] == "") {
 
                 // 次がインラインコードのときは, このまま処理を続ける.
@@ -1548,18 +1553,16 @@ class Parser {
                 //
                 // 逆にその次が, html要素のときは, <p></p>で囲まれてしまい,
                 // <p></p>で囲めない要素が来た時によろしくない.
+                //
+                // 次の文法外要素を本文法で囲む必要があるときはこのまま処理を続けるといい.
                 if (($context->morphSequence->nextMorph !== null) && $context->morphSequence->nextMorph["isInlineCode"]) {
-
+                    // \Debug::Log(['A', $currentMorph, $context->morphSequence->nextMorph]);
                 } else {
-                    // 次がhtml要素などはこのまま処理を続けない.
-                    // \Debug::Log('asad');
+                    // 次が本文法で囲む必要のない要素(html要素など)はこのまま処理を続けない.
+                    // \Debug::Log(['B', $currentMorph, $context->morphSequence->nextMorph]);
                     continue;
                 }
-                //continue;
             }
-
-            // 行頭の前処理
-            $output .= static::CallbackEventFuncs(static::$onPreBeginLineParserFuncList, $context);
 
             // 行頭の処理
             $output .= static::CallbackEventFuncs(static::$onBeginLineParserFuncList, $context);
